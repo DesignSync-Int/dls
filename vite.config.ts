c4e95cc00c6d4c45bf401c249ed102ae5dc13cce
skumar <kumasachin@gmail.com>
2025-08-05 14:17:03 +0100

    package update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist',
+      include: ['src/**/*'],
+      exclude: ['src/**/*.test.*', 'src/**/*.stories.*', 'src/main.tsx', 'src/App.tsx'],
+      insertTypesEntry: true,
+    }),
     visualizer({
       filename: 'dist/bundle-analysis.html',
       open: false,
